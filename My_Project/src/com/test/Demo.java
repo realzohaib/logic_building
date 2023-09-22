d951ae46683e910d797c0caa6a642569f4ac9bb5
realzohaib <itszohaib29@gmail.com>
2023-09-22 15:16:08 +0530

    123

--- My_Project/src/com/test/Demo.java
+++ My_Project/src/com/test/Demo.java
@@ -1,20 +1,18 @@
 package com.test;
+
 public class Demo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		System.out.println("program to print prine nym");
-<<<<<<< HEAD
-		
+
 		int a = 2;
-		int b=4;
-=======
-
-	       System.out.println("new method");
-
->>>>>>> 589d682cc1061ae4620b5349f289095367580a09
+		int b = 4;
 		
+		
+		System.out.println("new method");
+
 	}
 
 }
