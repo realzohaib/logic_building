90f5db733577e1c0ba1827b93aa2cc036f96058d
realzohaib <116001539+realzohaib@users.noreply.github.com>
2023-09-22 14:36:50 +0530

    Update Demo.java
    removed all lines

--- My_Project/src/com/test/Demo.java
+++ My_Project/src/com/test/Demo.java
@@ -3,22 +3,7 @@ public class Demo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello  world");
-<<<<<<< HEAD
-				System.out.println("hello  world");
-
-				System.out.println("hello  world");
-				System.out.println("hello  world");
-				
-				System.out.println("hello  world");
-				System.out.println("hello  world");
-		
-=======
-			
->>>>>>> 8def1bf454335324ec23e43c700ceb69d3dcd6b5
-		
-
-		
+		System.out.println("hello  world");		
 
 	}
 
