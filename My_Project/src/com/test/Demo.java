dff7ff0bd6d483fcf8aa2466b2610a6d7832fc3b
realzohaib <itszohaib29@gmail.com>
2023-09-22 14:59:06 +0530

    new program

--- My_Project/src/com/test/Demo.java
+++ My_Project/src/com/test/Demo.java
@@ -3,22 +3,8 @@ public class Demo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-				
-				System.out.println(" this line belongs to us");
-
-
-<<<<<<< HEAD
-		System.out.println("hello  world");		
 		
-		System.out.println("new feature");		
-
-=======
-		System.out.println("hello  world");	
-		System.out.println("my name is zohaib");
->>>>>>> 4e830f6f2ea1983c6c194d7ad2a1459fd03a926c
-
-
+		System.out.println("program to print prine nym");
 	}
 
 }
