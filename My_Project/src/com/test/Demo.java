4d6626fa5b8cdb7b0fafaa3bbadf2b43281ca784
realzohaib <itszohaib29@gmail.com>
2023-09-22 14:41:33 +0530

    this line belomgd to us

--- My_Project/src/com/test/Demo.java
+++ My_Project/src/com/test/Demo.java
@@ -4,7 +4,6 @@ public class Demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello  world");
-<<<<<<< HEAD
 				System.out.println("hello  world");
 
 				System.out.println("hello  world");
@@ -12,10 +11,12 @@ public class Demo {
 				
 				System.out.println("hello  world");
 				System.out.println("hello  world");
-		
-=======
-			
->>>>>>> 8def1bf454335324ec23e43c700ceb69d3dcd6b5
+				
+				System.out.println(" this line belongs to us");
+
+				
+				
+
 		
 
 		
